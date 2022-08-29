bbcb2e2b3008a7775ad07d4b1034ee7e59191774
Your Name <naimur202163@gmail.com>
2022-08-29 15:39:26 +0600

    hello world

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './redux/store.js';
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -12,4 +11,3 @@ root.render(
   </Provider>
 );
 
-reportWebVitals();
