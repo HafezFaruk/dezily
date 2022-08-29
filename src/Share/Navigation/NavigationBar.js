7fd5457ba4ce0db1915a114431248b0d220c504d
HafezFaruk <hmomarfaruk1454@gmail.com>
2022-08-29 19:10:04 +0600

    components add

--- src/Share/Navigation/NavigationBar.js
+++ src/Share/Navigation/NavigationBar.js
@@ -1,74 +1,34 @@
-import React from "react";
-
+import React from 'react'
+import { FiPhoneCall } from "react-icons/fi";
 function NavigationBar() {
   return (
-    <nav className="px-2 bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
-      <div className="container flex flex-wrap justify-between items-center mx-auto">
-        <a href="d" className="flex items-center">
+    <div className="bg-slate-600 p-3">
+      <nav className="mx-20 flex items-center justify-between text-[#fff]">
+        <div>
           <img
+            className=""
             src="https://themetechmount.com/html/dezily/images/logo-img.svg"
-            className="mr-3 h-6 sm:h-10"
-            alt="Logo"
+            alt="logo"
           />
-        </a>
-        <button
-          data-collapse-toggle="navbar-multi-level"
-          type="button"
-          className="inline-flex justify-center items-center ml-3 text-gray-400 rounded-lg md:hidden hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-blue-300 dark:text-gray-400 dark:hover:text-white dark:focus:ring-gray-500"
-          aria-controls="navbar-multi-level"
-          aria-expanded="false"
-        >
-          <span className="sr-only">Open main menu</span>
-          <svg
-            className="w-6 h-6"
-            aria-hidden="true"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-              clip-rule="evenodd"
-            ></path>
-          </svg>
-        </button>
-        <div
-          className="hidden w-full md:block md:w-auto"
-          id="navbar-multi-level"
-        >
-          <ul className="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-         
-
-            <li>
-              <a
-                href="d"
-                className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                Services
-              </a>
-            </li>
-            <li>
-              <a
-                href="d"
-                className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                Pricing
-              </a>
-            </li>
-            <li>
-              <a
-                href="d"
-                className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                Contact
-              </a>
-            </li>
-          </ul>
         </div>
-      </div>
-    </nav>
+        <ul className="flex justify-between">
+          <li className="mx-4">Home</li>
+          <li className="mx-4">Page</li>
+          <li className="mx-4">services</li>
+          <li className="mx-4">portfolio</li>
+          <li className="mx-4">blog</li>
+          <li className="mx-4">contact</li>
+        </ul>
+        <div className='flex items-center'>
+          <FiPhoneCall className="text-[#33D687] w-10 h-10" />
+          <div className='ml-3'>
+            <p>Call Us Now!</p>
+            <p>0284599657</p>
+          </div>
+        </div>
+      </nav>
+    </div>
   );
 }
 
-export default NavigationBar;
+export default NavigationBar
