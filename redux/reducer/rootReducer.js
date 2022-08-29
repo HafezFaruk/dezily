46099e44cbd960622b230f122b64efc544c0104e
Your Name <naimur202163@gmail.com>
2022-08-29 15:51:09 +0600

    updated

--- redux/reducer/rootReducer.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux";
-import { doctorReducer } from "./doctorReducer.js";
-const rootReducer = combineReducers({ doctorReducer })
-
-export default rootReducer;
-
