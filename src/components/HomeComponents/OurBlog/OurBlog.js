3c70d5141a4e212a01d36339c1b27d4d1bb109ec
Your Name <naimur202163@gmail.com>
2022-09-01 09:51:50 +0600

    working on context

--- src/components/HomeComponents/OurBlog/OurBlog.js
+++ src/components/HomeComponents/OurBlog/OurBlog.js
@@ -13,23 +13,29 @@ export default function OurBlog() {
     },
     {
       image: "https://themetechmount.com/html/dezily/images/blog/blog-16.jpg",
-      title: "Hard Content We Decide Ourselves A Intently Heart Transplant",
+      title: "Hard Content We Decide Ourselves A Intently Heart ",
     },
   ];
   return (
-    <div className="container mx-auto py-16">
+    <div className="">
+      <div className="container mx-auto py-16"></div>
       <p className="text-center text-md font-bold text-green">READ OUR BLOG</p>
-      <h2 className="text-4xl text-dark font-bold py-1 my-4" >
+      <h2 className="text-4xl text-dark font-bold py-1 my-4">
         We Provide Special Service <br /> For Patients
       </h2>
       <div className="flex space-x-3">
         {items.map((x) => (
           <div>
             <div>
-              <img src={x.image} />
+              <img className="object-fill" src={x.image} />
             </div>
-            <div>
-              <h2>{x.title}</h2>
+            <div className="">
+              <h2 className="px-8 text-2xl font-bold text-dark hover:text-green">{x.title}</h2>
+              <div>
+                <p className="text-light-gray py-2 mt-1 px-8 text-start">KIMOX / HEART SURGERY</p>
+              </div>
+              <hr className=""/>
+              <p className="text-start px-8 ">Read More</p>
             </div>
           </div>
         ))}
