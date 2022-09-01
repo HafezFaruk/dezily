e72edb983d8f72bf6dba14d426de6d2290e23052
Your Name <naimur202163@gmail.com>
2022-09-01 18:46:54 +0600

    working on health section

---
+++ src/components/HomeComponents/Health/Health.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Health() {
+  return (
+    <div>Health</div>
+  )
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Banner from "../components/HomeComponents/Banner/Banner";
 import ContactUs from "../components/HomeComponents/ContactUs/ContactUs";
+import Health from "../components/HomeComponents/Health/Health";
 import HomeService from "../components/HomeComponents/HomeService/HomeService";
 import OurBlog from "../components/HomeComponents/OurBlog/OurBlog";
 import ServiceProvide from "../components/HomeComponents/ServiceProvide/ServiceProvide";
@@ -11,6 +12,7 @@ function Home() {
     <div className="bg">
       <ContactUs />
       <ServiceProvide />
+      <Health/>
       <OurBlog />
     </div>
   );
