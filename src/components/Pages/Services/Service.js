c7b92a8d80a36f02dbdaeed65602d58309c5c021
ashrafulislampro <ashrafulislam1659@gmail.com>
2022-09-02 09:54:49 +0600

    about page creation is completed

--- src/components/Pages/Services/Service.js
+++ src/components/Pages/Services/Service.js
@@ -1,5 +1,6 @@
 import React from "react";
 import HeaderFeature from "../about/HeaderFeature";
+import Team from "./Team";
 
 const Service = () => {
   return (
@@ -7,6 +8,7 @@ const Service = () => {
       <div className="h-[32rem]">
         <HeaderFeature tab="Services" />
       </div>
+      <Team />
     </div>
   );
 };
---
+++ src/components/Pages/Services/Team.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Team = () => {
+  return (
+    <div className="container mx-auto">
+      <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div>
+          <img src="https://i.ibb.co/9vw9z8J/team-01.jpg" alt="" />
+        </div>
+        <div>
+          <img src="https://i.ibb.co/RpSFBQk/03.jpg" alt="" />
+        </div>
+        <div>
+          <img src="https://i.ibb.co/YhqctPk/02.jpg" alt="" />
+        </div>
+        <div>
+          <img src="https://i.ibb.co/J31BfGx/04.jpg" alt="" />
+        </div>
+        <div>
+          <img src="https://i.ibb.co/YhNZqjB/05.jpg" alt="" />
+        </div>
+        <div>
+          <img src="https://i.ibb.co/DQ6c0B0/06.jpg" alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Team;
