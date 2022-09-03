12ed7c0299fcc7b8a2cb085a138455065db274f7
Your Name <naimur202163@gmail.com>
2022-09-03 07:59:01 +0600

    home banner added

--- src/components/BlogComponents/TopBanner/TopBanner.js
+++ src/components/BlogComponents/TopBanner/TopBanner.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from "react";
 
 export default function TopBanner() {
   return (
-    <div className='bg-blogBanner'>
-      <h2 className='text-center text-white font-bold text-'>Blog</h2>
-      <p className='text-center'>Home/Blog</p>
+    <div className="bg-blogBanner md:bg-cover bg-no-repeat md:h-100">
+      <div className="p-20 md:p-60">
+        <h2 className="text-center  text-white text-3xl md:text-4xl font-bold text-">Blog</h2>
+        <p className="text-center text-white font-semibold text-lg">Home/Blog</p>
+      </div>
     </div>
-  )
+  );
 }
