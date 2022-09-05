4e6f8af2deafb8992a8e83b0776823d54dc89645
Your Name <naimur202163@gmail.com>
2022-09-05 15:20:06 +0600

    working on contact pagep

---
+++ src/components/ContactComponents/ContactBanner/ContactBanner.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import HeaderFeature from './../../Pages/about/HeaderFeature';
+
+export default function ContactBanner() {
+  return (
+    <div className="bg-blogBanner md:bg-cover bg-no-repeat md:h-100">
+    <div className="h-[28rem]">
+      <HeaderFeature tab="Contact " />
+    </div>
+  </div>
+  )
+}
