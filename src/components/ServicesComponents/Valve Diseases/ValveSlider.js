7a8e36436f51c47425c9319b7d6ba8854e6751f8
ridoy113 <almamounhridoy113@gmail.com>
2022-09-05 18:11:42 +0600

    valve slider maximum compplite

--- src/components/ServicesComponents/Valve Diseases/ValveSlider.js
+++ src/components/ServicesComponents/Valve Diseases/ValveSlider.js
@@ -8,19 +8,66 @@ import { valveData } from './valveData';
 
 
 const ValveSlider = () => {
+
+
+    const settings = {
+        dots: true,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        initialSlide: 0,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    initialSlide: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    };
+
+
     return (
         <div>
-            {valveData.map(item => (
-                <div className="bg-gradient-to-tr from-gray-700 to-gray-600 w-full relative h-[28rem] bg-cover bg-center">
-                    <img
-                        className="w-full absolute mix-blend-overlay h-full bg-center object-cover"
-                        src="{item.img}"
-                        alt=""
-                    />
-                    <p>{item.name}</p>
-                    <h1>{item.title}</h1>
-                </div>
-            ))}
+
+            <Slider {...settings}>
+
+                {valveData.map(item => (
+                    <div className='px-[1rem]'>
+                        <div className="bg-gradient-to-tr from-zinc-700 to-zinc-600 w-full relative h-[18rem] bg-cover bg-center">
+                            <img
+                                className="w-full absolute mix-blend-overlay h-full bg-center object-cover"
+                                src={item.img}
+                                alt=""
+                            />
+                            <div className='p-[2rem]'>
+                                <p className='text-[0.9rem] font-medium text-[#0de67d] pb-[0.6rem]'>{item.name}</p>
+                                <h1 className='text-[1.4rem] font-semibold text-white leading-[1.5rem]'>{item.title}</h1>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+
+            </Slider>
 
         </div>
     )
