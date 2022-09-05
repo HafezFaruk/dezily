2b21080cb2aff98342ebe4b147810afadc7204d0
Your Name <naimur202163@gmail.com>
2022-09-05 09:32:49 +0600

    blog section added

--- src/components/BlogComponents/GridBanner/GridBanner.js
+++ src/components/BlogComponents/GridBanner/GridBanner.js
@@ -1,15 +1,11 @@
 import React from "react";
+import HeaderFeature from './../../Pages/about/HeaderFeature';
 
 export default function GridBanner() {
   return (
-    <div className="bg-apple md:bg-cover bg-no-repeat md:h-100">
-      <div data-aos="fade-up" className="p-20 md:p-52">
-        <h2 className="text-center  text-white text-3xl md:text-4xl font-bold text-">
-          Blog-Single
-        </h2>
-        <p className="text-center text-white font-semibold text-lg">
-          Home/Blog
-        </p>
+    <div className="bg-apple  ">
+      <div className="h-[28rem]">
+        <HeaderFeature tab="Blog" />
       </div>
     </div>
   );
