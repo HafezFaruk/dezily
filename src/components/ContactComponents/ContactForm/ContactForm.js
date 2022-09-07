cac3ef95f9b813450aaf96d43dcc6e0ad368bd5f
Your Name <naimur202163@gmail.com>
2022-09-07 09:54:22 +0600

    submit button added

--- src/components/ContactComponents/ContactForm/ContactForm.js
+++ src/components/ContactComponents/ContactForm/ContactForm.js
@@ -4,12 +4,31 @@ export default function ContactForm() {
   return (
     <div className="bg-service-color">
       <div className="container mx-auto">
+        <p className="text text-green text-center font-semibold py-10">
+          Send Message
+        </p>
+        <h2 className="text-center text-[3rem] text-dark font-semibold">
+          Drop Your Links To Our <br />
+          <span>Experts</span>{" "}
+        </h2>
         <div>
-          <div class="grid grid-cols-3 gap-4">
-            <div className="col-span-2">01</div>
-            {/* <!-- ... --> */}
-            <div>09</div>
-          </div>
+          <form className="">
+            <div class="grid grid-cols-3 gap-4">
+              <div className="col-span-2  bg-white">
+                <div class="grid grid-cols-2 gap-4 p-5  ">
+                  <input type="text" placeholder="Your Name" />
+                  <input type="text" placeholder="Your Name" />
+                  <input type="text" placeholder="Your Name" />
+                  <input type="text" placeholder="Your Name" />
+                  <textarea className="col-span-2" placeholder="Message"></textarea>
+                  <button className="text-start">Submit</button>
+                </div>
+                
+              </div>
+              {/* <!-- ... --> */}
+              <div className=" bg-white">Call-Section</div>
+            </div>
+          </form>
         </div>
       </div>
     </div>
