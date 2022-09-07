5171d02b0d69e7c4ed805d2ecee44089d6cf74cc
Your Name <naimur202163@gmail.com>
2022-09-07 09:08:31 +0600

    gird added

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ContactBanner from "../ContactComponents/ContactBanner/ContactBanner";
+import ContactForm from "../ContactComponents/ContactForm/ContactForm";
 function Contact() {
   return (
     <div>
     <ContactBanner/>
+    <ContactForm/>
     </div>
   );
 }
---
+++ src/components/ContactComponents/ContactForm/ContactForm.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function ContactForm() {
+  return (
+    <div className="bg-service-color">
+      <div className="container mx-auto">
+        <div>
+          <div class="grid grid-cols-3 gap-4">
+            <div className="col-span-2">01</div>
+            {/* <!-- ... --> */}
+            <div>09</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
