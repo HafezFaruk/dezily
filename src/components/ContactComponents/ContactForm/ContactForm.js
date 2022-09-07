81109345a906612999fcef9bcb0156a965abd2ab
Your Name <naimur202163@gmail.com>
2022-09-07 10:19:02 +0600

    service added

--- src/components/ContactComponents/ContactForm/ContactForm.js
+++ src/components/ContactComponents/ContactForm/ContactForm.js
@@ -13,24 +13,60 @@ export default function ContactForm() {
         </h2>
         <div>
           <form className="">
-            <div class="grid grid-cols-3 gap-4">
-              <div className="col-span-2  bg-white">
-                <div class="grid grid-cols-2 gap-4 p-5  ">
-                  <input type="text" placeholder="Your Name*" />
-                  <input type="text" placeholder="Email*" />
-                  <input type="text" placeholder="Phone Number*" />
-                  <input type="text" placeholder="Subjects*" />
+            <div class="md:grid grid-cols-3 gap-4 py-[10rem]">
+              <div className="col-span-2  bg-white h-[40rem]">
+                <div class="grid grid-cols-2 gap-4 p-10  ">
+                  <input
+                    className="border-none bg-service-color border-r h-[4rem]"
+                    type="text"
+                    placeholder="Your Name*"
+                  />
+                  <input
+                    className="border-none bg-service-color border-r h-[4rem] "
+                    type="text"
+                    placeholder="Email*"
+                  />
+                  <input
+                    className="border-none bg-service-color border-r h-[4rem] "
+                    type="text"
+                    placeholder="Phone Number*"
+                  />
+                  <input
+                    className="border-none bg-service-color border-r h-[4rem] "
+                    type="text"
+                    placeholder="Subjects*"
+                  />
                   <textarea
-                    className="col-span-2"
+                    className="col-span-2  bg-service-color border-none h-[19rem] "
+                    type="text"
                     placeholder="Message"
                   ></textarea>
                   <div>
-                    <button>Submit</button>
+                    <button className="text-white bg-green px-6 py-2">Submit</button>
                   </div>
                 </div>
               </div>
               {/* <!-- ... --> */}
-              <div className=" bg-white">Call-Section</div>
+              <div className=" bg-white">
+                <div>
+                  <div>
+                    <img src="https://i.ibb.co/SJHchgB/doctor.png"/>
+                  </div>
+                  <div></div>
+                </div>
+                <div>
+                  <div>
+                    <img src="https://i.ibb.co/gVqg24m/defibrillator.png"/>
+                  </div>
+                  <div></div>
+                </div>
+                <div>
+                  <div>
+                    <img src="https://i.ibb.co/FKrF5jk/heart.png"/>
+                  </div>
+                  <div></div>
+                </div>
+              </div>
             </div>
           </form>
         </div>
