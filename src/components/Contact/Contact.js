8909c4a5ad35c94f4f45987da3fde56f91d3ca6b
Your Name <naimur202163@gmail.com>
2022-09-07 10:43:44 +0600

    Contact page done

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -2,12 +2,14 @@ import React from "react";
 import ContactBanner from "../ContactComponents/ContactBanner/ContactBanner";
 import ContactForm from "../ContactComponents/ContactForm/ContactForm";
 import SingleImage from "../ContactComponents/SingleImage/SingleImage";
+import Clients from "../HomeComponents/Clients/Clients";
 function Contact() {
   return (
     <div>
       <ContactBanner />
       <ContactForm />
       <SingleImage />
+      <Clients/>
     </div>
   );
 }
