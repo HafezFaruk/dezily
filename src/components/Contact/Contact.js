433defa3df902f8dfa86aefe4f00becd4bd6857d
Your Name <naimur202163@gmail.com>
2022-09-07 10:42:21 +0600

    single image added

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ContactBanner from "../ContactComponents/ContactBanner/ContactBanner";
 import ContactForm from "../ContactComponents/ContactForm/ContactForm";
+import SingleImage from "../ContactComponents/SingleImage/SingleImage";
 function Contact() {
   return (
     <div>
-    <ContactBanner/>
-    <ContactForm/>
+      <ContactBanner />
+      <ContactForm />
+      <SingleImage />
     </div>
   );
 }
