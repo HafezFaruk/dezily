27ce21be82ad0a2c0eeac0571c0020674a41baaa
Your Name <naimur202163@gmail.com>
2022-09-08 09:51:29 +0600

    all fetch able data added

--- src/components/BlogCategories/CardioConsultancy.js
+++ src/components/BlogCategories/CardioConsultancy.js
@@ -1,5 +1,49 @@
 import React from "react";
 
 export default function CardioConsultancy() {
+  const aboutDezily = [
+    {
+      image: "https://i.ibb.co/4Rs4cJD/single-img-14.jpg",
+      title: "Heart Surgery Specialist Expert Doctors",
+      headtext:
+        "We have the top level of world best heart surgery specialist expert doctors. We are used latest technology in our healthcare.",
+      subtitle: "ABOUT Dezily",
+      subimage: "https://i.ibb.co/DQHL29s/single-img-01.jpg",
+      content:
+        "Contrary to popular belief, Lorem Ipsum is not ly randomtext. It has roots in a piece of classical Lterature from 45 BC, making it over 2000 . Richard McClintock, a Latin professor at Hampden-Sydin Virginia",
+    },
+  ];
+
+  const solutions = [
+    {
+      image: "https://i.ibb.co/x1wMFwS/service-09.png",
+      title: "SURGERY",
+      content: "Aneurysm repair",
+    },
+    {
+      image: "https://i.ibb.co/nsPBR8m/service-10.png",
+      title: "SURGERY",
+      content: "MAZE SURGERY",
+    },
+    {
+      image: "https://i.ibb.co/LPzhr1v/service-11.png",
+      title: "SURGERY",
+      content: "HEART TRANSPLANT",
+    },
+    {
+      image: "https://i.ibb.co/YRJDZSH/service-12.png",
+      title: "SURGERY",
+      content: "BYPAS SURGARY",
+    },
+  ];
+
+  const choseUsText = [
+    {
+      subtitle: "WHY CHOOSE US",
+      title: "Cardiologists Are Doctors Who Treating Diseases",
+      content:
+        "A surgical procedure used in which the blocked portion of the coronary artery is bypassed with another piece of blood vessel.",
+    },
+  ];
   return <div>CardioConsultancy</div>;
 }
